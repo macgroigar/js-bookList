20a789b7d73f2fa3b6512697e74f88b7f86edd04
macgroigar <stephenmacgregor4@gmail.com>
2022-08-07 15:48:29 +0100

    add book logic

--- app.js
+++ app.js
@@ -89,3 +89,40 @@ class Store {
         localStorage.setItem('books', JSON.stringify(books));
     }
 }
+
+// Events to display a book in the list
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+
+// Event to add a book in the list
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    
+    // prevent default action
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+       UI.showAlert('Please fill in all the fields', 'danger');
+    } else {
+        // Instatiate a book
+        const book = new Book(title, author, isbn);
+
+        // Add book to list
+        UI.addBookToList(book);
+
+        // add a book to strage
+        Store.addBook(book);
+
+        // Successfully added a book alert
+        UI.showAlert('Your book has been added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }
+
+});
